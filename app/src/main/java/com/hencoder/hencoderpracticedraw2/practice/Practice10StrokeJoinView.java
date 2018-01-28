52e99fb94349c9286ba3da1b98352369c01dded1
xxtlant <genius248@163.com>
2018-01-28 15:06:58 +0800

    finish 04-10

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice10StrokeJoinView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice10StrokeJoinView.java
@@ -40,16 +40,19 @@ public class Practice10StrokeJoinView extends View {
 
         // 使用 Paint.setStrokeJoin() 来设置不同的拐角形状
 
+        paint.setStrokeJoin(Paint.Join.MITER);
         canvas.translate(100, 100);
         // 第一种形状：MITER
         canvas.drawPath(path, paint);
 
         canvas.translate(300, 0);
         // 第二种形状：BEVEL
+        paint.setStrokeJoin(Paint.Join.BEVEL);
         canvas.drawPath(path, paint);
 
         canvas.translate(300, 0);
         // 第三种形状：ROUND
+        paint.setStrokeJoin(Paint.Join.ROUND);
         canvas.drawPath(path, paint);
 
         canvas.restore();
